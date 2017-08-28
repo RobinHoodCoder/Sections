7d95045901e559bb0c2abad2daf69d627c4ca5e0
Terrra-Robin <robin@terra-it.com>
2017-08-28 16:17:27 +0200

    Test

--- templates/basic/sections.php
+++ templates/basic/sections.php
@@ -1,4 +1,5 @@
 <h1 class="main"><?=$page['title'] ?></h1>
+<h1 class="main"><?=$page['title'] ?></h1>
 <?
 foreach ($page_sections as $section) {
 	include ('../templates/callouts/' . $section['section_type'] . '.php');
